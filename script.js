26031521b1a6f5044964f820525a8faad181d61a
FajarHosting <82355656+FajarHosting@users.noreply.github.com>
2025-03-22 16:08:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById("contact-form").addEventListener("submit", function(event) {
     event.preventDefault();
     
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let message = document.getElementById("message").value;
+    let name = document.getElementById("developer web").value;
+    let email = document.getElementById("fajarsetiawan12322@gmail.com").value;
+    let message = document.getElementById("pesan").value;
 
     if (name && email && message) {
         alert("Terima kasih, " + name + "! Pesan Anda telah dikirim.");
